12146555a303dfa5d6178f285e5abf27a67b6588
Christopher <c.q.ezzard@gmail.com>
2023-02-06 22:51:19 -0600

    Updates to user model and controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class UserController {
     @GetMapping("/api/users")
     public List<User> getUsers(){
         return service.findAllUsers();
+    }
+
+    @PostMapping("/api/createAccount")
+    public User createNewUser(User newUser){
+        return service.createUsers(newUser);
     }
 }
