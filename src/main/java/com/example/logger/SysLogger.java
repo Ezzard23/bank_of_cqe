451738a670d2f11dc69cbddffb3782551681ff50
Christopher <c.q.ezzard@gmail.com>
2023-02-23 13:14:10 -0600

    System Logger

--- src/main/java/com/example/logger/SysLogger.java
+++ src/main/java/com/example/logger/SysLogger.java
@@ -1,5 +1,42 @@
 package com.example.logger;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class SysLogger {
+    static Logger logger = Logger.getLogger(SysLogger.class.getName());
     
+    public static void main(String[] args) {
+        try {
+            LogManager.getLogManager().readConfiguration(new FileInputStream("mylogging.properties"));
+        } catch (SecurityException | IOException e1) {
+            e1.printStackTrace();
+        }
+        logger.setLevel(Level.FINE);
+        logger.addHandler(new ConsoleHandler());
+        //adding custom handler
+        logger.addHandler(new SysHandler());
+        try {
+            //FileHandler file name with max size and number of log files limit
+            Handler fileHandler = new FileHandler("/Users/pankaj/tmp/logger.log", 2000, 5);
+            fileHandler.setFormatter(new LogFormatter());
+            //setting custom filter for FileHandler
+            fileHandler.setFilter(new LogFilter());
+            logger.addHandler(fileHandler);
+            
+            for(int i=0; i<1000; i++){
+                //logging messages
+                logger.log(Level.INFO, "Msg"+i);
+            }
+            logger.log(Level.CONFIG, "Config data");
+        } catch (SecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
