9668337fed5dfc19378f24c41c46791d0847d34d
Christopher <c.q.ezzard@gmail.com>
2023-02-23 13:07:34 -0600

    Logger reformatting

--- src/main/java/com/example/logger/SysLogger.java
+++ src/main/java/com/example/logger/SysLogger.java
@@ -1,26 +1,5 @@
 package com.example.logger;
 
-import java.util.logging.LogRecord;
-import java.util.logging.StreamHandler;
-
-public class SysLogger extends StreamHandler {
-
-    @Override
-    public void publish(LogRecord record) {
-        //add own logic to publish
-        super.publish(record);
-    }
-
-
-    @Override
-    public void flush() {
-        super.flush();
-    }
-
-
-    @Override
-    public void close() throws SecurityException {
-        super.close();
-    }
+public class SysLogger {
     
 }
